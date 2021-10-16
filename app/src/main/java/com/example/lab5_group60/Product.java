019acae25cff97d9d67076b8dbe87dfaeef17351
djtoastman14 <71289356+djtoastman14@users.noreply.github.com>
2021-10-16 16:06:18 -0400

    improved product

--- app/src/main/java/com/example/lab5_group60/Product.java
+++ app/src/main/java/com/example/lab5_group60/Product.java
@@ -3,11 +3,24 @@ package com.example.lab5_group60;
 public class Product {
     String name;
     double price;
+    String id;
+
+    public Product(String id, String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.id = id;
+    }
 
     public Product(String name, double price) {
         this.name = name;
         this.price = price;
     }
+
+    public void setId(String id) {
+        this.id = id; }
+
+    public String getId() {
+        return id; }
 
     public void setName(String name) {
         this.name = name; }
